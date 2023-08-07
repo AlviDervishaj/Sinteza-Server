334af4303cdf432aa61ae4f9dd987b4d5588df5c
Alvi Dervishaj <alvidervishaj9@gmail.com>
2023-08-07 17:37:00 +0200

    Merged devices and processes together

--- server.ts
+++ server.ts
@@ -2,7 +2,6 @@ import express, { Express } from 'express';
 import http from 'http';
 import { Server, Socket } from "socket.io";
 import { Processes } from './controllers/Processes';
-import { Devices } from './controllers/Devices';
 import cors from "cors";
 
 
@@ -16,11 +15,9 @@ const io = new Server(server, {
 });
 
 const _processes = new Processes();
-const _devices = new Devices();
 
 io.on('connection', (socket: Socket) => {
   _processes.add(socket);
-  _devices.add(socket);
 });
 
 server.listen(3030, () => {
